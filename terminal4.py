fa97a64124a18f9a22e7a9d7c4b2a349d7839406
oumeanin <136037784+oumeanin@users.noreply.github.com>
2025-08-25 13:29:39 +0900

    Create terminal4.py

---
+++ terminal4.py
@@ -0,0 +1,4 @@
+sudo apt install -y ros-foxy-gscam \
+                   gstreamer1.0-tools gstreamer1.0-plugins-base \
+                   gstreamer1.0-plugins-good gstreamer1.0-plugins-bad \
+                   gstreamer1.0-plugins-ugly
