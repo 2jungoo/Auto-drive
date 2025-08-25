b99d89678063d6e5af79b21c6dced5ecf6fc40b2
oumeanin <136037784+oumeanin@users.noreply.github.com>
2025-08-25 13:44:47 +0900

    Update terminal2.py

--- terminal2.py
+++ terminal2.py
@@ -1,3 +1,1 @@
-pkill -f image_transport || true
-sudo apt-get update && sudo apt-get install -y ros-foxy-image-transport-plugins
 ros2 run image_transport republish raw in:=/image_raw compressed out:=/image_raw
