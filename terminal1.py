ee493c9218c789c0c1042402f785f00f0f18ff29
oumeanin <136037784+oumeanin@users.noreply.github.com>
2025-08-25 13:44:32 +0900

    Update terminal1.py

--- terminal1.py
+++ terminal1.py
@@ -1,9 +1,6 @@
-pkill -f gscam || true
-pkill -f gst-launch || true
-
+sudo systemctl restart nvargus-daemon
 ros2 run gscam gscam_node --ros-args \
   -p gscam_config:="nvarguscamerasrc sensor-id=0 bufapi-version=true ! \
-video/x-raw(memory:NVMM),width=1280,height=720,framerate=30/1 ! \
+video/x-raw\(memory:NVMM\),width=1280,height=720,framerate=30/1 ! \
 nvvidconv ! video/x-raw,format=BGRx ! videoconvert ! \
-video/x-raw,format=BGR ! appsink drop=true max-buffers=1 sync=false" \
-  -p frame_id:=camera_link -p sync_sink:=false
+video/x-raw,format=BGR ! appsink drop=true max-buffers=1 sync=false"
